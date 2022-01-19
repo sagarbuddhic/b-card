bd364a67b1ea0540ae361e094f6f02e06f0f238d
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-01-19 15:14:37 +0530

    fix: edit form

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 /** @jsx jsx */ import { jsx } from "@emotion/core";
-import Button from "react-bootstrap/Button";
-import Label from "react-bootstrap/FormLabel";
 import logo from "./logo.svg";
 import Form from "react-bootstrap/Form";
-import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
-import Badge from "react-bootstrap/Badge";
 import "./App.css";
 import * as styles from "./App.style";
-import { HelpBlock } from "react-bootstrap";
-
-//     <Form noValidate>
-//   <Container>
-//      <Row>
-//        <Col>
-//           <Image thumbnail />
-//        </Col>
-//        <Col>
-//           <span ref={firstNameLabel}>
-//           </span>
-//           <span ref={lastNameLabel}>
-//           </span>
-//        </Col>
-//      </Row>
-//   </Container>
-// </Form>
-//  <Form noValidate>
-//     <Form.Row>
-//       <Form.Group controlId="validationFormik05">
-//         <Form.Label ref={firstNameLabel}></Form.Label>
-//       </Form.Group>
-//     </Form.Row>container
-//     <Form.Row>
-//       <Form.Group controlId="validationFormik05">
-//         <Form.Label ref={lastNameLabel}></Form.Label>
-//       </Form.Group>
-//     </Form.Row>
-//     <Button type="submit">Submit form</Button>
-// </Form>
 
 function App() {
   const firstNameRef = useRef(null);
@@ -53,10 +19,25 @@ function App() {
   const businessNameRef = useRef(null);
   const websiteRef = useRef(null);
   const addressRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <div css={styles.container}>
       <Form css={styles.form}>
+      <Form.Row>
+      <Form.Group as={Col} controlId="Business Name">
+            <Form.Label>Business Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="businessName"
+              required
+              placeholder=""
+              onChange={event => {
+                businessNameRef.current.textContent = event.target.value;
+              }}
+            />
+          </Form.Group>
+      </Form.Row>
         <Form.Row>
           <Form.Group as={Col} controlId="firstName">
             <Form.Label>First Name</Form.Label>
@@ -83,6 +64,19 @@ function App() {
             />
           </Form.Group>
         </Form.Row>
+        <Form.Row>
+          <Form.Group as={Col} controlId="profession">
+            <Form.Label>Profession</Form.Label>
+            <Form.Control
+              type="text"
+              name="profession"
+              placeholder="eg Realtor"
+              onChange={event => {
+                professionRef.current.textContent = event.target.value;
+              }}
+            />
+          </Form.Group>
+          </Form.Row>
         <Form.Row>
           <Form.Group as={Col} controlId="mobile">
             <Form.Label>Mobile</Form.Label>
@@ -119,31 +113,8 @@ function App() {
               }}
             />
           </Form.Group>
-          <Form.Group as={Col} controlId="profession">
-            <Form.Label>Profession</Form.Label>
-            <Form.Control
-              type="text"
-              name="profession"
-              placeholder="eg Realtor"
-              onChange={event => {
-                professionRef.current.textContent = event.target.value;
-              }}
-            />
-          </Form.Group>
         </Form.Row>
         <Form.Row>
-          <Form.Group as={Col} controlId="Business Name">
-            <Form.Label>Business Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="businessName"
-              required
-              placeholder=""
-              onChange={event => {
-                businessNameRef.current.textContent = event.target.value;
-              }}
-            />
-          </Form.Group>
           <Form.Group as={Col} controlId="Website">
             <Form.Label>Website</Form.Label>
             <Form.Control
@@ -172,29 +143,38 @@ function App() {
       </Form>
       <Card css={styles.card}>
         <Row>
-          <Col sm={4}>
-            <Image css={styles.image} />
+          <Col sm={5} css={styles.imageContainer} >
+          {selectedImage && (
+            <Image alt="" css={styles.image} src={URL.createObjectURL(selectedImage)} />
+          )}
+      <br />
+      <br /> 
+            {!selectedImage && <input
+                 type="file"
+                name="myImage"
+                onChange={(event) => {
+                setSelectedImage(event.target.files[0]);
+            }}
+            />}
           </Col>
-          <Col sm={8}>
-            <Card.Body>
-              <h2>
-                <Badge variant="secondary">
-                  <Card.Text ref={businessNameRef}></Card.Text>
-                </Badge>
-              </h2>
+          <Col sm={7}>
+            <Card.Body css={styles.cardBody}>
+              <div  css={styles.businessNameWrapper}>
+              <Card.Text css={styles.businessName} ref={businessNameRef} />
+              </div>
+              <Card.Text css={styles.name}>
+                <span ref={firstNameRef}></span>
+                <span> </span>
+                <span ref={lastNameRef}></span>
+              </Card.Text>
+              <Card.Text css={styles.profession} ref={professionRef} />
               <div css={styles.contactDetails}>
                 <span ref={mobileRef}></span>
                 <span ref={officeRef}></span>
                 <span ref={emailRef}></span>
                 <span ref={websiteRef}></span>
               </div>
-              <Card.Text ref={addressRef}></Card.Text>
-              <Card.Text>
-                <span ref={firstNameRef}></span>
-                <span> </span>
-                <span ref={lastNameRef}></span>
-              </Card.Text>
-              <Card.Text ref={professionRef}></Card.Text>
+              <Card.Text css={styles.address} ref={addressRef}></Card.Text>
             </Card.Body>
           </Col>
         </Row>
--- src/App.style.js
+++ src/App.style.js
@@ -19,30 +19,73 @@ export const appHeader = css`
 
 export const container = css`
   margin: 20px;
-  display: flex;
-  flex-flow: "row wrap";
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-gap: 50px;
   justify-content: space-evenly;
 `;
 
 export const form = css`
-  border: 0.5px gray solid;
-  padding: 15px;
+  border-radius: 5px;
+  padding: 20px;
+  background-color: #f2f2f2;
+`;
+
+export const imageContainer = css`
+  background-color: #FFFEE4;
+  height: 350px;
+  width: 250px;
 `;
 
 export const image = css`
-  background-color: grey;
-  height: 400px;
+  height: 350px;
   width: 250px;
 `;
 
 export const card = css`
   padding: 15px;
   height: 400px;
-  width: 800px;
+  width: 700px;
+  background-color: #FFFEE4;
+`;
+
+export const cardBody = css`
+  padding-right: 70px;
+`;
+
+export const businessNameWrapper = css`
+  padding-bottom: 40px;
+`;
+
+export const businessName = css`
+  font-size: 40px;
+  font-family: Cursive;
+`;
+
+export const name = css`
+  font-size: 20px;
+  font-family: Monospace;
+  line-height: 0.8;
+`;
+
+export const profession = css`
+  font-style: italic;
+  font-family: Fantasy;
+  font-size: 18px;
+  line-height: 0.8;
+  padding-bottom: 30px;
 `;
 
 export const contactDetails = css`
   display: flex;
   flex-direction: column;
-  font-size: 18px;
+  font-size: 15px;
+  font-family: Serif;
+  font-weight: bold;
+  padding-bottom: 30px;
+`;
+
+export const address = css`
+  font-size: 15px;
+  font-family: Serif;
 `;
