c6ec81940351745b16e86153f511c3bf4b900999
Sagar Buddhi <sagar.buddhi@domain.com.au>
2022-01-19 20:04:24 +0530

    Updates

---
+++ precache-manifest.cb016c26f5b091f78d17d25adf7bf9fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ebdeac1184e3ddcf693ad38e088a0b2",
+    "url": "/b-card/index.html"
+  },
+  {
+    "revision": "ef7bcf3f14673e2e6ac2",
+    "url": "/b-card/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a5cb5241f6ceac9b2f64",
+    "url": "/b-card/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ef7bcf3f14673e2e6ac2",
+    "url": "/b-card/static/js/2.aa4b9005.chunk.js"
+  },
+  {
+    "revision": "a5cb5241f6ceac9b2f64",
+    "url": "/b-card/static/js/main.93b897d7.chunk.js"
+  },
+  {
+    "revision": "7493b4b0e7bf0b431799",
+    "url": "/b-card/static/js/runtime-main.922dc605.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/b-card/static/media/logo.25bf045c.svg"
+  }
+]);
